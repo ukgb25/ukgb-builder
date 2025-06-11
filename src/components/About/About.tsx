3f792efc29c63b1da169a0089cf21699e7ad5b35
Keyur Patel <keyur2523@gmail.com>
2025-06-11 23:10:23 +0100

    chore: test build

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -16,7 +16,7 @@ const About = (props: Props) => {
             <div className="col-lg-6">
               <div className="about-content">
                 <h4>
-                  WE ARE ON EXPERT <br />
+                  WE ARE ON NOT EXPERT <br />
                   THIS FIELD BUILDINGS
                   <br />
                   SOLUTIONS SINCE 1974.
